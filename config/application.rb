3e382c03654a5f725b37ea02f32b4ba88d42a5c9
key-sn <k.passtd@gmail.com>
2018-03-26 00:51:50 +0900

    ローカルでのサンプル作成

--- config/application.rb
+++ config/application.rb
@@ -11,13 +11,13 @@ module RailsVueSandbox
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.template_engine :slim
+      g.test_framework :rspec, view_specs: false, helper_specs: false, fixture: false 
+    end
   end
 end
 
-config.generators do |g|
-  g.stylesheets false
-  g.javascripts false
-  g.helper false
-  g.template_engine :slim
-  g.test_framework :rspec, view_specs: false, helper_specs: false, fixture: false 
-end
